7bad5cdb1cb2ea7e497bb1b4555e452f4ea8b684
ShadowFusion <54732272+ShadowFusion@users.noreply.github.com>
2020-02-06 20:52:27 -0500

    Add files via upload

--- ShadowAIO.lua
+++ ShadowAIO.lua
@@ -26,7 +26,7 @@ local TargetSelector
 -- [ AutoUpdate ] --
 do
     
-    local Version = 0.11
+    local Version = 0.12
     
     local Files = {
         Lua = {
@@ -80,7 +80,7 @@ local Champions = {
     ["Jax"] = true,
     ["Amumu"] = true,
     ["Cassiopeia"] = true,
-    ["Graves"] = true,
+    ["Nocturne"] = true,
 }
 
 --Checking Champion 
@@ -169,6 +169,11 @@ local function OnEnemyHeroLoad(cb)
         end
     end
 end
+
+function GetCastLevel(unit, slot)
+	return unit:GetSpellData(slot).level == 0 and 1 or unit:GetSpellData(slot).level
+end
+
 if myHero.charName == "Urgot" then
 class "Urgot"
 function Urgot:__init()
@@ -1708,3 +1713,165 @@ if myHero.charName == "Graves" then
         end
     end
     end
+
+    if myHero.charName == "Nocturne" then
+        class "Nocturne"
+        function Nocturne:__init()
+            
+            self.Q = {Type = _G.SPELLTYPE_LINE, Delay = 0.25, Radius = 100, Range = 1200, Speed = 1600, Collision = false, MaxCollision = 1, CollisionTypes = {_G.COLLISION_MINION, _G.COLLISION_ENEMYHERO}}
+            self.W = {Type = _G.SPELLTYPE_CIRCLE, Delay = 0.25, Radius = 800, Range = 800, Speed = 1400, Collision = false, MaxCollision = 0, CollisionTypes = {_G.COLLISION_MINION, _G.COLLISION_ENEMYHERO}}
+            self.E = {Type = _G.SPELLTYPE_CIRCLE, Delay = 0.25, Radius = 0, Range = 475, Speed = 0, Collision = true, MaxCollision = 0, CollisionTypes = {_G.COLLISION_MINION, _G.COLLISION_ENEMYHERO}}
+            self.R = {Type = _G.SPELLTYPE_CIRCLE, Delay = 0.50, Radius = 10000, Range = ({2500, 3250, 4000})[GetCastLevel(myHero, _R)], Speed = 2000, Collision = false, MaxCollision = 1, CollisionTypes = {_G.COLLISION_MINION, _G.COLLISION_ENEMYHERO}}
+            
+            print(GetCastLevel(myHero, _R))
+
+            OnAllyHeroLoad(function(hero)
+                Allys[hero.networkID] = hero
+            end)
+            
+            OnEnemyHeroLoad(function(hero)
+                Enemys[hero.networkID] = hero
+            end)
+            
+            Callback.Add("Tick", function() self:Tick() end)
+            Callback.Add("Draw", function() self:Draw() end)
+            
+            orbwalker:OnPreMovement(
+                function(args)
+                    if lastMove + 180 > GetTickCount() then
+                        args.Process = false
+                    else
+                        args.Process = true
+                        lastMove = GetTickCount()
+                    end
+                end
+            )
+        end
+        
+        function Nocturne:LoadMenu()
+            self.shadowMenu = MenuElement({type = MENU, id = "shadowNocturne", name = "Shadow Nocturne"})
+            self.shadowMenu:MenuElement({type = MENU, id = "combo", name = "Combo"})
+            self.shadowMenu.combo:MenuElement({id = "Q", name = "Use Q in Combo", value = true})
+            self.shadowMenu.combo:MenuElement({id = "W", name = "Use W in Combo", value = false})
+            self.shadowMenu.combo:MenuElement({id = "E", name = "Use E in  Combo", value = true})
+            self.shadowMenu:MenuElement({type = MENU, id = "jungleclear", name = "Jungle Clear"})
+            self.shadowMenu.jungleclear:MenuElement({id = "UseQ", name = "Use Q in Jungle Clear", value = true})
+            self.shadowMenu.jungleclear:MenuElement({id = "UseE", name = "Use E in Jungle Clear", value = true})
+            self.shadowMenu:MenuElement({type = MENU, id = "laneclear", name = "Lane Clear"})
+            self.shadowMenu.laneclear:MenuElement({id= "UseQLane", name = "Use Q in Lane Clear", value = true})
+            self.shadowMenu:MenuElement({type = MENU, id = "autor", name = "Kill Steal"})
+            self.shadowMenu.autor:MenuElement({id = "AutoR", name = "Kill steal with R", value = true})
+            --self.shadowMenu:MenuElement({type = MENU, id = "jungleclear", name = "Jungle Clear"})
+        end
+        
+        function Nocturne:Draw()
+            
+        end
+        
+        function Nocturne:Tick()
+            if myHero.dead or Game.IsChatOpen() or (ExtLibEvade and ExtLibEvade.Evading == true) then
+                return
+            end
+            self:AutoR()
+            if orbwalker.Modes[0] then
+                self:Combo()
+            elseif orbwalker.Modes[3] then
+                self:jungleclear()
+                self:laneclear()
+            end
+        end
+        
+        function Nocturne:AutoR()
+            local target = TargetSelector:GetTarget(self.R.Range, 1)
+            if target and IsValid(target) then
+            local rdmg = (({150, 275, 400})[myHero:GetSpellData(_R).level or 1] + (myHero.bonusDamage * 2))
+            if Ready(_R) and target and IsValid(target) and (target.health <= rdmg) and self.shadowMenu.autor.AutoR:Value() then
+                local Pred = GamsteronPrediction:GetPrediction(target, self.R, myHero)
+                --print(Pred.Hitchance)
+                    --Control.CastSpell(HK_Q, target)
+                    Control.CastSpell(HK_R)
+                    self:CastR(target)
+            end
+        end
+        end
+        
+        
+        function Nocturne:Combo()
+            local QPred = GamsteronPrediction:GetPrediction(target, self.Q, myHero)
+            local target = TargetSelector:GetTarget(self.Q.Range, 1)
+            if Ready(_Q) and target and IsValid(target) then
+                if self.shadowMenu.combo.Q:Value() then
+                    --Control.CastSpell(HK_Q, target)
+                    self:CastQ(target)
+                end
+            end
+            print(GetCastLevel(myHero, _R))
+            print(self.R.Range)
+            if Ready(_W) then
+                if self.shadowMenu.combo.W:Value() then
+                    Control.KeyDown(HK_W)
+                end
+            end
+            
+            local target = TargetSelector:GetTarget(self.E.Range - 100, 1)
+            if Ready(_E) and target and IsValid(target) then
+                if self.shadowMenu.combo.E:Value() then
+                    Control.CastSpell(HK_E, target)
+                    --self:CastSpell(HK_Etarget)
+                end
+            end
+        
+        end
+        
+        function Nocturne:jungleclear()
+        if self.shadowMenu.jungleclear.UseQ:Value() then 
+            for i = 1, Game.MinionCount() do
+                local obj = Game.Minion(i)
+                if obj.team ~= myHero.team then
+                    if obj ~= nil and obj.valid and obj.visible and not obj.dead then
+                        if Ready(_Q) and self.shadowMenu.jungleclear.UseQ:Value() and obj and obj.team == 300 and obj.valid and obj.visible and not obj.dead and (obj.pos:DistanceTo(myHero.pos) < 800) then
+                            Control.CastSpell(HK_Q, obj);
+                        end
+                    end
+                end
+                if Ready(_E) and self.shadowMenu.jungleclear.UseE:Value() and obj and obj.team == 300 and obj.valid and obj.visible and not obj.dead and obj.pos:DistanceTo(myHero.pos) < 125 + myHero.boundingRadius then
+                    Control.CastSpell(HK_E, obj);
+                end
+            end
+        end
+        end
+
+        function Nocturne:laneclear()
+            for i = 1, Game.MinionCount() do
+                local minion = Game.Minion(i)
+                if minion.team ~= myHero.team then 
+                    local dist = myHero.pos:DistanceTo(minion.pos)
+                    if self.shadowMenu.laneclear.UseQLane:Value() and Ready(_Q) and dist <= self.Q.Range then 
+                        Control.CastSpell(HK_Q, minion.pos)
+                    end
+
+                end
+            end
+        end
+        
+        function Nocturne:CastQ(target)
+            if Ready(_Q) and lastQ + 350 < GetTickCount() and orbwalker:CanMove() then
+                local Pred = GamsteronPrediction:GetPrediction(target, self.Q, myHero)
+                if Pred.Hitchance >= _G.HITCHANCE_NORMAL then
+                    Control.CastSpell(HK_Q, Pred.CastPosition)
+                    lastQ = GetTickCount()
+                end
+            end
+        end
+        
+        function Nocturne:CastR(target)
+            if Ready(_R) and lastR + 350 < GetTickCount() and orbwalker:CanMove() then
+                local Pred = GamsteronPrediction:GetPrediction(target, self.R, myHero)
+                if Pred.Hitchance >= _G.HITCHANCE_NORMAL then
+                    Control.CastSpell(HK_R, Pred.CastPosition)
+                    lastR = GetTickCount()
+                end
+            end
+        end
+        end
+        
