ba51e32b048ecbb15187e3d838fc8ff56809bbb3
ShadowFusion <54732272+ShadowFusion@users.noreply.github.com>
2020-04-13 11:40:13 -0400

    Create ShadowAIOSupport.lua

---
+++ ShadowAIOSupport.lua
@@ -0,0 +1,318 @@
+local GameHeroCount = Game.HeroCount
+local GameHero = Game.Hero
+print(myHero.charName)
+local myHero = myHero
+local LocalGameTimer = Game.Timer
+local GameMissile = Game.Missile
+local GameMissileCount = Game.MissileCount
+
+local lastQ = 0
+
+local lastW = 0
+local lastE = 0
+local lastR = 0
+local lastIG = 0
+local lastMove = 0
+local HITCHANCE_NORMAL = 2
+local HITCHANCE_HIGH = 3
+local HITCHANCE_IMMOBILE = 4
+
+local Enemys = {}
+local Allys = {}
+
+local orbwalker
+local TargetSelector
+
+-- [ AutoUpdate ] --
+do
+    
+    local Version = 0.16
+    
+    local Files = {
+        Lua = {
+            Path = SCRIPT_PATH,
+            Name = "ShadowAIO.lua",
+            Url = "https://raw.githubusercontent.com/ShadowFusion/MJGA/master/ShadowAIOSupport.lua"
+        },
+        Version = {
+            Path = SCRIPT_PATH,
+            Name = "ShadowAIO.version",
+            Url = "https://raw.githubusercontent.com/ShadowFusion/MJGA/master/ShadowAIOSupport.version"    -- check if Raw Adress correct pls.. after you have create the version file on Github
+        }
+    }
+    
+    local function AutoUpdate()
+        
+        local function DownloadFile(url, path, fileName)
+            DownloadFileAsync(url, path .. fileName, function() end)
+            while not FileExist(path .. fileName) do end
+        end
+        
+        local function ReadFile(path, fileName)
+            local file = io.open(path .. fileName, "r")
+            local result = file:read()
+            file:close()
+            return result
+        end
+        
+        DownloadFile(Files.Version.Url, Files.Version.Path, Files.Version.Name)
+        local textPos = myHero.pos:To2D()
+        local NewVersion = tonumber(ReadFile(Files.Version.Path, Files.Version.Name))
+        if NewVersion > Version then
+            DownloadFile(Files.Lua.Url, Files.Lua.Path, Files.Lua.Name)
+            print("New ShadowAIO Vers. Press 2x F6")     -- <-- you can change the massage for users here !!!!
+        else
+            print(Files.Version.Name .. ": No Updates Found")   --  <-- here too
+        end
+    
+    end
+    
+    AutoUpdate()
+
+end
+
+
+
+local Champions = {
+    ["Blitzcrank"] = true, 
+}
+
+--Checking Champion 
+if Champions[myHero.charName] == nil then
+    print('Shadow AIO does not support ' .. myHero.charName) return
+end
+
+
+Callback.Add("Load", function()
+    orbwalker = _G.SDK.Orbwalker
+    TargetSelector = _G.SDK.TargetSelector
+    if FileExist(COMMON_PATH .. "GamsteronPrediction.lua") then
+        require('GamsteronPrediction');
+    else
+        print("Requires GamsteronPrediction please download the file thanks!");
+        return
+    end
+      
+    require('DamageLib')
+    myHero.charName:LoadMenu();
+end)
+
+local function IsValid(unit)
+    if (unit
+        and unit.valid
+        and unit.isTargetable
+        and unit.alive
+        and unit.visible
+        and unit.networkID
+        and unit.health > 0
+        and not unit.dead
+    ) then
+    return true;
+end
+return false;
+end
+
+local function MinionsNear(pos,range)
+	local pos = pos.pos
+	local N = 0
+		for i = 1, Game.MinionCount() do 
+		local Minion = Game.Minion(i)
+		local Range = range * range
+		if IsValid(Minion, 800) and Minion.team == TEAM_ENEMY and GetDistanceSqr(pos, Minion.pos) < Range then
+			N = N + 1
+		end
+	end
+	return N	
+end	
+
+local function GetAllyHeroes() 
+	AllyHeroes = {}
+	for i = 1, Game.HeroCount() do
+		local Hero = Game.Hero(i)
+		if Hero.isAlly and not Hero.isMe then
+			table.insert(AllyHeroes, Hero)
+		end
+	end
+	return AllyHeroes
+end
+
+local function Ready(spell)
+    return myHero:GetSpellData(spell).currentCd == 0 and myHero:GetSpellData(spell).level > 0 and myHero:GetSpellData(spell).mana <= myHero.mana and Game.CanUseSpell(spell) == 0
+end
+
+local function OnAllyHeroLoad(cb)
+    for i = 1, GameHeroCount() do
+        local obj = GameHero(i)
+        if obj.isAlly then
+            cb(obj)
+        end
+    end
+end
+
+local function OnEnemyHeroLoad(cb)
+    for i = 1, GameHeroCount() do
+        local obj = GameHero(i)
+        if obj.isEnemy then
+            cb(obj)
+        end
+    end
+end
+
+function GetCastLevel(unit, slot)
+	return unit:GetSpellData(slot).level == 0 and 1 or unit:GetSpellData(slot).level
+end
+
+local function GetStatsByRank(slot1, slot2, slot3, spell)
+	local slot1 = 0
+    local slot2 = 0
+    local slot3 = 0
+	return (({slot1, slot2, slot3})[myHero:GetSpellData(spell).level or 1])
+end
+
+
+if myHero.charName == "Blitzcrank" then
+class "Blitzcrank"
+function Blitzcrank:__init()
+    
+    self.Q = {Type = _G.SPELLTYPE_CIRCLE, Delay = 0.25, Radius = 60, Range = 800, Speed = 1400, Collision = true, MaxCollision = 0, CollisionTypes = {_G.COLLISION_MINION, _G.COLLISION_ENEMYHERO}}
+    self.W = {Type = _G.SPELLTYPE_CIRCLE, Delay = 0.25, Radius = 800, Range = 800, Speed = 1400, Collision = true, MaxCollision = 0, CollisionTypes = {_G.COLLISION_MINION, _G.COLLISION_ENEMYHERO}}
+    self.E = {Type = _G.SPELLTYPE_LINE, Delay = 0.25, Radius = 0, Range = 475, Speed = 0, Collision = true, MaxCollision = 1, CollisionTypes = {_G.COLLISION_MINION, _G.COLLISION_ENEMYHERO}}
+    self.R = {Type = _G.SPELLTYPE_LINE, Delay = 0.50, Radius = 0, Range = 2500, Speed = 3200, Collision = true, MaxCollision = 1, CollisionTypes = {_G.COLLISION_MINION, _G.COLLISION_ENEMYHERO}}
+    
+    
+    OnAllyHeroLoad(function(hero)
+        Allys[hero.networkID] = hero
+    end)
+    
+    OnEnemyHeroLoad(function(hero)
+        Enemys[hero.networkID] = hero
+    end)
+    
+    Callback.Add("Tick", function() self:Tick() end)
+    Callback.Add("Draw", function() self:Draw() end)
+    
+    orbwalker:OnPreMovement(
+        function(args)
+            if lastMove + 180 > GetTickCount() then
+                args.Process = false
+            else
+                args.Process = true
+                lastMove = GetTickCount()
+            end
+        end
+    )
+end
+
+function Blitzcrank:LoadMenu()
+    self.shadowMenu = MenuElement({type = MENU, id = "shadowBlitzcrank", name = "Shadow Blitzcrank"})
+    self.shadowMenu:MenuElement({type = MENU, id = "combo", name = "Combo"})
+    self.shadowMenu.combo:MenuElement({id = "Q", name = "Use Q in Combo", value = true})
+    self.shadowMenu.combo:MenuElement({id = "W", name = "Use W in Combo(Recomended Disabled)", value = false})
+    self.shadowMenu.combo:MenuElement({id = "E", name = "Use E in  Combo", value = true})
+    self.shadowMenu:MenuElement({type = MENU, id = "jungleclear", name = "Jungle Clear"})
+    self.shadowMenu.jungleclear:MenuElement({id = "UseQ", name = "Use Q in Jungle Clear", value = true})
+    self.shadowMenu.jungleclear:MenuElement({id = "UseE", name = "Use E in Jungle Clear", value = true})
+    self.shadowMenu:MenuElement({type = MENU, id = "autor", name = "Auto R"})
+    self.shadowMenu.autor:MenuElement({id = "AutoR", name = "Auto R", value = true})
+    --self.shadowMenu:MenuElement({type = MENU, id = "jungleclear", name = "Jungle Clear"})
+end
+
+function Blitzcrank:Draw()
+    
+end
+
+function Blitzcrank:Tick()
+    if myHero.dead or Game.IsChatOpen() or (ExtLibEvade and ExtLibEvade.Evading == true) then
+        return
+    end
+    self:AutoR()
+    if orbwalker.Modes[0] then
+        self:Combo()
+    elseif orbwalker.Modes[3] then
+        self:jungleclear()
+    end
+end
+
+function Blitzcrank:AutoR()
+    local target = TargetSelector:GetTarget(self.R.Range, 1)
+    if Ready(_R) and target and IsValid(target) and (target.health <= target.maxHealth / 4) and self.shadowMenu.autor.AutoR:Value() then
+        local Pred = GamsteronPrediction:GetPrediction(target, self.R, myHero)
+        --print(Pred.Hitchance)
+            --Control.CastSpell(HK_Q, target)
+            self:CastR(target)
+    end
+end
+
+
+function Blitzcrank:Combo()
+    local QPred = GamsteronPrediction:GetPrediction(target, self.Q, myHero)
+    local target = TargetSelector:GetTarget(self.Q.Range, 1)
+    if Ready(_Q) and target and IsValid(target) then
+        if self.shadowMenu.combo.Q:Value() then
+            --Control.CastSpell(HK_Q, target)
+            self:CastQ(target)
+        end
+    end
+
+    local Wactive = false;
+    if myHero:GetSpellData(_W).name == 'BlitzcrankW2' then
+    Wactive = true
+    else
+    Wactive = false
+    end
+    local target = TargetSelector:GetTarget(self.W.Range, 1)
+    if Ready(_W) and target and IsValid(target) and Wactive == false then
+        if self.shadowMenu.combo.W:Value() then
+            Control.KeyDown(HK_W)
+            Control.KeyUp(HK_W)
+        end
+    end
+    
+    local target = TargetSelector:GetTarget(self.E.Range - 100, 1)
+    if Ready(_E) and target and IsValid(target) then
+        if self.shadowMenu.combo.E:Value() then
+            Control.CastSpell(HK_E, target)
+            --self:CastSpell(HK_Etarget)
+        end
+    end
+
+end
+
+function Blitzcrank:jungleclear()
+if self.shadowMenu.jungleclear.UseQ:Value() then 
+    for i = 1, Game.MinionCount() do
+        local obj = Game.Minion(i)
+        if obj.team ~= myHero.team then
+            if obj ~= nil and obj.valid and obj.visible and not obj.dead then
+                if Ready(_Q) and self.shadowMenu.jungleclear.UseQ:Value() and obj and obj.team == 300 and obj.valid and obj.visible and not obj.dead and (obj.pos:DistanceTo(myHero.pos) < 800) then
+                    Control.CastSpell(HK_Q, obj);
+                end
+            end
+        end
+        if Ready(_E) and self.shadowMenu.jungleclear.UseE:Value() and obj and obj.team == 300 and obj.valid and obj.visible and not obj.dead and obj.pos:DistanceTo(myHero.pos) < 125 + myHero.boundingRadius then
+            Control.CastSpell(HK_E, obj);
+        end
+    end
+end
+end
+
+function Blitzcrank:CastQ(target)
+    if Ready(_Q) and lastQ + 350 < GetTickCount() and orbwalker:CanMove() then
+        local Pred = GamsteronPrediction:GetPrediction(target, self.Q, myHero)
+        if Pred.Hitchance >= _G.HITCHANCE_NORMAL then
+            Control.CastSpell(HK_Q, Pred.CastPosition)
+            lastQ = GetTickCount()
+        end
+    end
+end
+
+function Blitzcrank:CastR(target)
+    if Ready(_R) and lastR + 350 < GetTickCount() and orbwalker:CanMove() then
+        local Pred = GamsteronPrediction:GetPrediction(target, self.R, myHero)
+        if Pred.Hitchance >= _G.HITCHANCE_NORMAL then
+            Control.CastSpell(HK_R, Pred.CastPosition)
+            lastR = GetTickCount()
+        end
+    end
+end
+end
