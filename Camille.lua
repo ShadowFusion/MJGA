417831f56f3d5ca0be1e499af2267a08eff7c0ed
ShadowFusion <54732272+ShadowFusion@users.noreply.github.com>
2020-05-14 07:19:19 -0400

    Update Camille.lua

--- Camille.lua
+++ Camille.lua
@@ -139,56 +139,6 @@ local function Ready(spell)
     return myHero:GetSpellData(spell).currentCd == 0 and myHero:GetSpellData(spell).level > 0 and myHero:GetSpellData(spell).mana <= myHero.mana and Game.CanUseSpell(spell) == 0
 end
 
-local function CanCast(spell, mode)
-    if spell == _Q then
-        if Ready(spell) and mode == 0 and self.Menu.Q.QCombo:Value() then
-            return true
-        end
-        if Ready(spell) and mode == 1 and self.Menu.Q.QHarass:Value()then
-            return true
-        end
-        if Ready(spell) and mode == 10 and self.Menu.Q.QAuto:Value()then
-            return true
-        end
-    end
-
-    if spell == _W then
-        if Ready(spell) and mode == 0 and self.Menu.W.WCombo:Value() then
-            return true
-        end
-        if Ready(spell) and mode == 1 and self.Menu.W.WHarass:Value()then
-            return true
-        end
-        if Ready(spell) and mode == 10 and self.Menu.W.WAuto:Value()then
-            return true
-        end
-    end
-
-    if spell == _E then
-        if Ready(spell) and mode == 0 and self.Menu.E.ECombo:Value() then
-            return true
-        end
-        if Ready(spell) and mode == 1 and self.Menu.E.EHarass:Value()then
-            return true
-        end
-        if Ready(spell) and mode == 10 and self.Menu.E.EAuto:Value()then
-            return true
-        end
-    end
-
-    if spell == _R then
-        if Ready(spell) and mode == 0 and self.Menu.R.RCombo:Value() then
-            return true
-        end
-        if Ready(spell) and mode == 1 and self.Menu.R.RHarass:Value()then
-            return true
-        end
-        if Ready(spell) and mode == 10 and self.Menu.R.RAuto:Value()then
-            return true
-        end
-    end
-    return false
-end
 
 local function OnAllyHeroLoad(cb)
     for i = 1, GameHeroCount() do
@@ -229,6 +179,19 @@ function IsImmobileTarget(unit)
 	return false	
 end
 
+local function ValidTarget(unit, range)
+    if (unit and unit.valid and unit.isTargetable and unit.alive and unit.visible and unit.networkID and unit.pathing and unit.health > 0) then
+        if range then
+            if (unit.pos:DistanceTo(myHero.pos) < range) then
+                return true;
+            end
+        else
+            return true
+        end
+    end
+    return false;
+end
+
 function IsFacing(unit)
     local V = Vector((unit.pos - myHero.pos))
     local D = Vector(unit.dir)
@@ -244,12 +207,13 @@ local Heroes = {"Camille"}
 if not table.contains(Heroes, myHero.charName) then return end
         
 class "Camille"
+
 function Camille:__init()
     
-    self.Q = {Type = _G.SPELLTYPE_CIRCLE, Radius = 150}
-    self.W = {Type = _G.SPELLTYPE_LINE, Range = 1450, Radius = 40.25, Speed = 3200, Collision = true, MaxCollision = 1, CollisionTypes = {_G.COLLISION_YASUOWALL, _G.COLLISION_MINION}}
-    self.E = {Type = _G.SPELLTYPE_CIRCLE, Range = 900, Radius = 50}
-    self.R = {Type = _G.SPELLTYPE_CIRCLE, Range = 20000, Radius = 225, Speed = 1500, Collision = true, MaxCollision = 1, CollisionTypes = {2, 3}}
+    self.Q = {Type = _G.SPELLTYPE_CIRCLE, Range = 325, Speed = 20, Collision = false}
+    self.W = {Type = _G.SPELLTYPE_CONE, Range = 610, Radius = 100, Speed = 1750, Collision = false}
+    self.E = {Type = _G.SPELLTYPE_CIRCLE, Range = 800, Speed = 500}
+    self.R = {Type = _G.SPELLTYPE_CIRCLE, Range = 475, Speed = 1200}
 
     
 
@@ -315,127 +279,37 @@ end
 
 
 function Camille:Draw()
-
-    if self.shadowMenu.Drawing.drawr:Value() and Ready(_R) then
-		Draw.Circle(myHero, 1500, 1, Draw.Color(255, 0, 0))
-		end                                                 
-		if self.shadowMenu.Drawing.drawe:Value() and Ready(_E) then
-		Draw.Circle(myHero, 900, 1, Draw.Color(235, 147, 52))
-		end
-		if self.shadowMenu.Drawing.draww:Value() and Ready(_W) then
-		Draw.Circle(myHero, 1450, 1, Draw.Color(0, 212, 250))
-        end
-
-    
+--[[
     local target = TargetSelector:GetTarget(20000, 5)
     if target and IsValid(target) then
     local rdmg = getdmg("R", target, myHero)
     if self.shadowMenu.Drawing.drawrkill:Value() and Ready(_R) and target.health < rdmg then
         Draw.Text("Killable with [R]", 18, target.pos2D.x, target.pos2D.y + 50, Draw.Color(255, 225, 255, 255))
     end
-end
-
-
+    ]]
 end
 
 function Camille:Tick()
     if myHero.dead or Game.IsChatOpen() or (ExtLibEvade and ExtLibEvade.Evading == true) then
         return
     end
-    self:autoe()
-    self:killsteal()
-    self:junglekillsteal()
-    if orbwalker.Modes[0] then
-        self:Combo()
-    elseif orbwalker.Modes[3] then
-    end
+    self:Logic()
 end
 
-function Camille:autoe()
-    local target = TargetSelector:GetTarget(self.E.Range, 1)
-    if target and IsValid(target) then
-    if Ready(_E) and self.shadowMenu.combo.EONCC:Value() and IsImmobileTarget(target) then
-        self:CastE(target)
 
-    end
-    end
-end
-function Camille:killsteal()
-    local target = TargetSelector:GetTarget(self.R.Range, 1)
-    if target and IsValid(target) then      
-    local d = myHero.pos:DistanceTo(target.pos)
-    local wdmg = getdmg("W", target, myHero)
-    local rdmg = getdmg("R", target, myHero)
-        if Ready(_R) and target and IsValid(target) and (target.health <= rdmg) and self.shadowMenu.killsteal.killstealr:Value() and d > 500 and d < self.shadowMenu.killsteal.killstealrangemax:Value() then
-            self:CastR(target)
-        end
-        if Ready(_W) and target and IsValid(target) and (target.health <= wdmg) and self.shadowMenu.killsteal.killstealw:Value() then
-            self:CastW(target)
-        end
-    end
-end
 
-function Camille:Combo()
+function Camille:Logic()
     local target = TargetSelector:GetTarget(self.W.Range, 1)
     if target == nil then return end
-    if Ready(_W) and target and IsValid(target) then
-        if self.shadowMenu.combo.W:Value() then
-           self:CastW(target)
-            --self:CastSpell(HK_Etarget)
-        end														---- you have "end" forget
+    if self:CanCast(_W, 0) and ValidTarget(target, self.W.Range)  then
+        self:CastW(target)
     end
 
-    local target = TargetSelector:GetTarget(self.E.Range, 1)
-    if target == nil then return end
-    local posBehind = myHero.pos:Extended(target.pos, target.distance + 100)
-    if Ready(_E) and target and IsValid(target) then
-        if self.shadowMenu.combo.E:Value() then
-            self:CastE(target)
-            --self:CastSpell(HK_Etarget)
-        end
-    end
-
-
-
-    
-    local distance = target.pos:DistanceTo(myHero.pos) 
     local target = TargetSelector:GetTarget(self.Q.Range, 1)
     if target == nil then return end
-    if Ready(_Q) and target and IsValid(target)then
-        if self.shadowMenu.combo.Q:Value() then
-            if distance > 615 and not self:HasSecondQ() or (distance < 615 and self:HasSecondQ()) then
-                Control.CastSpell(HK_Q)
-            end
-        end    
-    end 
-end
-
-function Camille:junglekillsteal()
-    if self.shadowMenu.junglekillsteal.W:Value() then 
-        for i = 1, Game.MinionCount() do
-            local obj = Game.Minion(i)
-            if obj.team ~= myHero.team then
-                if obj ~= nil and obj.valid and obj.visible and not obj.dead then
-                    local wdmg = getdmg("W", obj, myHero, 1)
-                    if Ready(_W) and self.shadowMenu.junglekillsteal.W:Value() and obj and obj.team == 300 and obj.valid and obj.visible and not obj.dead and (obj.pos:DistanceTo(myHero.pos) < self.W.Range and obj.health < wdmg) then
-                        Control.CastSpell(HK_W, obj);
-                    end
-                end
-            end
-        end
+    if self:CanCast(_Q, 0) and ValidTarget(target, self.Q.Range)  then
+        Control.CastSpell(HK_Q)
     end
-end
-
-function Camille:HasSecondQ()
-    return Camille:GotBuff(myHero, "CamilleQ") > 0
-end
-
-function Camille:GotBuff(unit, buffname)
-    for i = 0, unit.buffCount do
-        local buff = unit:GetBuff(i)
-        if buff and buff.name == buffname and buff.count > 0 then return buff.count end
-    end
-    return 0
 end
 
 function Camille:CastW(target)
@@ -466,4 +340,55 @@ function Camille:CastR(target)
             lastR = GetTickCount()
         end
     end
+end
+
+function Camille:CanCast(spell, mode)
+    if spell == _Q then
+        if Ready(spell) and mode == 0 and self.Menu.Q.QCombo:Value() then
+            return true
+        end
+        if Ready(spell) and mode == 1 and self.Menu.Q.QHarass:Value()then
+            return true
+        end
+        if Ready(spell) and mode == 10 and self.Menu.Q.QAuto:Value()then
+            return true
+        end
+    end
+
+    if spell == _W then
+        if Ready(spell) and mode == 0 and self.Menu.W.WCombo:Value() then
+            return true
+        end
+        if Ready(spell) and mode == 1 and self.Menu.W.WHarass:Value()then
+            return true
+        end
+        if Ready(spell) and mode == 10 and self.Menu.W.WAuto:Value()then
+            return true
+        end
+    end
+
+    if spell == _E then
+        if Ready(spell) and mode == 0 and self.Menu.E.ECombo:Value() then
+            return true
+        end
+        if Ready(spell) and mode == 1 and self.Menu.E.EHarass:Value()then
+            return true
+        end
+        if Ready(spell) and mode == 10 and self.Menu.E.EAuto:Value()then
+            return true
+        end
+    end
+
+    if spell == _R then
+        if Ready(spell) and mode == 0 and self.Menu.R.RCombo:Value() then
+            return true
+        end
+        if Ready(spell) and mode == 1 and self.Menu.R.RHarass:Value()then
+            return true
+        end
+        if Ready(spell) and mode == 10 and self.Menu.R.RAuto:Value()then
+            return true
+        end
+    end
+    return false
 end
